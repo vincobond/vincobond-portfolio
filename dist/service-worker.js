19e5a955a92bc9230ba928c6bab210f3eb1d6006
vincobond <microsoftinnotech@gmail.com>
2025-11-26 00:10:54 +0100

    hello
    hello

---
+++ dist/service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'vc-portfolio-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/static/css/main.css',
+    '/static/js/main.js'
+];
+
+// Install service worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+// Fetch with cache-first strategy
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+            )
+    );
+});
+
+// Clean up old caches
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
